a1a1d2385a2986dd6bcebdbe2143ad736e07b20f
Alexandre Bedard <alexandre@bedard.xyz>
2020-01-27 15:35:45 -0500

    Ex 04 complété

---
+++ rappel_dae1_s02_vs/04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04
+{
+    class Program
+    {
+        static int Primary(int arg)
+        {
+            int result = 0;
+
+            for (long i = 2; i <= arg; i = i + 1)
+            {
+                if ((i % 2 != 0) || (i == 2))
+                {
+                    result += arg;
+                }
+            }
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result {Primary(number)}");
+        }
+    }
+}
