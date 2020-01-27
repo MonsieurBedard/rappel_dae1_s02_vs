b9f1ab851b92b13ca14b8dd59d2cbefdb7c64dce
Alexandre Bedard <alexandre@bedard.xyz>
2020-01-27 14:32:26 -0500

    Ex 02 complété

---
+++ rappel_dae1_s02_vs/01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Alexandre Bédard";
+            Console.WriteLine(name);
+        }
+    }
+}
