a071046d77607e4774ad04bbc7f8d748fe107e8d
Alexandre Bedard <alexandre@bedard.xyz>
2020-01-27 15:42:22 -0500

    Ex 05 complété

---
+++ rappel_dae1_s02_vs/05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 9, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            int result = 0;
+
+            foreach (int e in array)
+            {
+                result += e;
+            }
+
+            Console.WriteLine($"result: {result}");
+        }
+    }
+}
